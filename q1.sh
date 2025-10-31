20b85872c8b4843b33b1cc0b3e70c9f890b4c24f
mint686 <runyamasram.2322@gmail.com>
2025-10-31 16:31:28 +0530

    Add files via upload

---
+++ q1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Prime numbers between 1 and 100:"
+
+for (( num=2; num<=100; num++ ))
+do
+    is_prime=1
+    for (( i=2; i*i<=num; i++ ))
+    do
+        if [ $((num % i)) -eq 0 ]
+        then
+            is_prime=0
+            break
+        fi
+    done
+    if [ $is_prime -eq 1 ]
+    then
+        echo -n "$num "
+    fi
+done
+echo
+
+
+
